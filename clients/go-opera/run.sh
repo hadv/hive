8895ddad87350c47f415f6422367d6af1a5283e0
Ha ĐANG <dvietha@gmail.com>
2023-02-08 23:44:29 +0700

    using genesis json to init some accounts balance for testing

--- clients/go-opera/run.sh
+++ clients/go-opera/run.sh
@@ -3,8 +3,13 @@
 # Immediately abort the script on any error encountered
 set -e
 
+# Configure the chain.
+mv /genesis.json /genesis-input.json
+jq -f /mapper.jq /genesis-input.json > /genesis.json
+
 # Just try to run the fakenet with just one validator (which will work practically as a PoA blockchain)
 /opera --fakenet=1/1 \
+    --init=/genesis.json \
     --nat=none \
     --http --http.addr="0.0.0.0" \
     --http.corsdomain="*" \
